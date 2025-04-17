c8e5e8f238107e75d055df0c3080637b0895575e
vishaltrycode <vishaltrycode@gmail.com>
2025-04-17 17:03:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accoundId = 12345;
+let accountEmail = 'asjdh@gmail.com'
+var accountPassword = '213564'
+let accountIs;
+
+console.table([accoundId, accountEmail, accountPassword, accountIs])
